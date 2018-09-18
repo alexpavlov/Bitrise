9f9da17d671134654f8b8e89ddc8a697687d4fe1
Alex Pavlov <alex@rushproject.com>
2018-09-18 10:22:30 -0400

    Adds stock project

---
+++ Bitrise/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Bitrise
+//
+//  Created by Alex Pavlov on 9/18/18.
+//  Copyright © 2018 Alex Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
